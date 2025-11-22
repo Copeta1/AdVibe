e53f4148058f221094f898f81a8bd125a6273c99
Copeta1 <pecolajrobert1@gmail.com>
2025-11-22 21:42:32 +0100

    feat: Finalna priprema za deployment - dodan CORS i start script

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,20 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
+const ALLOWED_ORIGIN = process.env.FRONTEND_URL || "http://localhost:3000";
+
 const app = express();
+
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: ALLOWED_ORIGIN,
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use(express.json());
 
@@ -18,4 +31,5 @@ app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on Port ${PORT}`);
+  console.log(`CORS is allowing: ${ALLOWED_ORIGIN}`);
 });
